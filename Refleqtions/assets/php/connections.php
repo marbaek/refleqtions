0631432fae35d29b7f473d3bcb3eb528a1d9bb0f
marbaek <152411297+marbaek@users.noreply.github.com>
2025-08-05 00:58:42 +0800

    Update connections.php

--- Refleqtions/assets/php/connections.php
+++ Refleqtions/assets/php/connections.php
@@ -2,7 +2,7 @@
 // Establish a connection to the MySQL database
 // Establish a connection to the MySQL database
 $login_connections = mysqli_connect("sql100.infinityfree.com", "if0_38727199", "VsFSJXlEbI", "if0_38727199_logindb");
-$crud_connections = mysqli_connect("sql100.infinityfree.com", "if0_38727199", "VsFSJXlEbI", "if0_38727199_logindb")
+$crud_connections = mysqli_connect("sql100.infinityfree.com", "if0_38727199", "VsFSJXlEbI", "if0_38727199_logindb");
 
 // Check the connection
 if (!$login_connections || !$crud_connections) {
